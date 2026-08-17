1bcc27c99f12e0e0e837d218a44ea1c97b5b33dc
Hrithik <hrithiknonia66@gmail.com>
2026-08-17 16:03:09 +0530

    shorted link creation complete

--- app/repositories/url_repository/url_repository.py
+++ app/repositories/url_repository/url_repository.py
@@ -10,11 +10,7 @@ class UrlRepository:
         result = await links_collection.insert_one(link_data_dict)
         return await links_collection.find_one({"_id": result.inserted_id})
 
-    async def find_existing_link(self, original_url: str, user_id: str | None) -> dict | None:
-        return await links_collection.find_one({
-            "original_url": original_url,
-            "user_id": user_id
-        })
+    
 
 
 url_repository = UrlRepository()
